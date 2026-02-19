7e49dc7b10b621a37096a614a48d47c48141bbee
Ilan Peer <ilan.peer@intel.com>
2026-02-19 22:25:01 +0200

    NAN: Add security support in NAF transmit flow
    1. Add NAN security attributes when needed.
    2. Perform pre-Tx frames processing based on the current
       state of the NDP state machine and frame subtype.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>

--- src/nan/nan.c
+++ src/nan/nan.c
@@ -1069,6 +1069,10 @@ static int nan_action_build(struct nan_data *nan, struct nan_peer *peer,
 	if (ret)
 		return ret;
 
+	ret = nan_sec_add_attrs(nan, peer, subtype, buf);
+	if (ret)
+		return ret;
+
 	ret = nan_ndl_add_avail_attrs(nan, peer, buf);
 	if (ret)
 		return ret;
@@ -1104,6 +1108,10 @@ static int nan_action_send(struct nan_data *nan, struct nan_peer *peer,
 		return -1;
 
 	ret = nan_action_build(nan, peer, subtype, buf);
+	if (ret)
+		goto out;
+
+	ret = nan_sec_pre_tx(nan, peer, buf);
 	if (ret)
 		goto out;
 
