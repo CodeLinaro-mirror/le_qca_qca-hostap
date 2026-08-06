greearb/hostap-ct

commit 1da95f8fc70144e853200fcb2b400b44819bec22

trace: Free prg_fname on program exit
This removes one more reachable memory allocation from memory tracing.

Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

src/utils/trace.c
@@ -423,6 +423,9 @@ void wpa_trace_check_ref(const void *addr)
 
 void wpa_trace_deinit(void)
 {
+	free(prg_fname);
+	prg_fname = NULL;
+
 #ifdef WPA_TRACE_BFD
 	free(syms);
 	syms = NULL;

CodeLinaro-mirror/le_qca_qca-hostap

commit eaa647bb8c9efb45e1162f39599b627f1f855311

trace: Guard prg_fname cleanup with CONFIG_WPA_TRACE_BFD
prg_fname is declared only when CONFIG_WPA_TRACE_BFD is enabled, but its
cleanup in wpa_trace_deinit() is executed unconditionally. This causes a
compilation failure when CONFIG_WPA_TRACE_BFD is disabled.

Move the prg_fname free() call into the WPA_TRACE_BFD conditional
block to keep its usage consistent with the declaration and prevent
build failures when CONFIG_WPA_TRACE_BFD is disabled.

Fixes: 1da95f8fc701 ("trace: Free prg_fname on program exit")
Signed-off-by: Raj Kumar Bhagat <raj.bhagat@oss.qualcomm.com>

src/utils/trace.c
@@ -423,10 +423,10 @@ void wpa_trace_check_ref(const void *addr)
 
 void wpa_trace_deinit(void)
 {
+#ifdef WPA_TRACE_BFD
 	free(prg_fname);
 	prg_fname = NULL;
 
-#ifdef WPA_TRACE_BFD
 	free(syms);
 	syms = NULL;
 	if (cached_abfd) {