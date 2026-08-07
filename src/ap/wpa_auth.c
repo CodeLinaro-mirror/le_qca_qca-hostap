sailfishos-mirror/hostap

commit 2a9129858375126d5d2859e918c5a8c52cf8821a

EPPKE: RSNE/Key Delivery element in (Re)Association Response frame
Include RSNE and Key Delivery element indicating group KDEs to an EPP
non-AP STA in the (Re)Association Response frame and also set the
protected bit in the frame control in the IEEE 802.11 frame header as an
indication to the driver/firmware that this frame needs to be encrypted.

Signed-off-by: Rohan Dutta <drohan@qti.qualcomm.com>
Signed-off-by: Sai Pratyusha Magam <sai.magam@oss.qualcomm.com>

src/ap/wpa_auth.c
@@ -7941,6 +7941,75 @@ bool wpa_auth_sm_known_sta_identification(struct wpa_state_machine *sm,
 }
 
 
+#ifdef CONFIG_ENC_ASSOC
+
+u8 * wpa_auth_eid_key_delivery(u8 *eid, size_t max_len,
+			       struct wpa_state_machine *sm, bool is_ml)
+{
+	size_t gtk_len, kde_len = 0;
+	u8 rsc[WPA_KEY_RSC_LEN] = {0}, *gtk;
+	struct wpa_group *gsm = sm->group;
+	u8 hdr[2];
+
+	/* TODO: Make sure there is sufficient length for the element and also
+	 * support fragmentation of the Key Delivery element. */
+
+	/* ElementID(0xff)|Length(1B)|ElementID EXtn(1B)|RSC(8B)|KDE list */
+	*eid++ = WLAN_EID_EXTENSION;
+
+	if (!is_ml) {
+		/* GTK KDE: 0xdd|len(1B)|RSN Selector(4B)|KeyID(2B)|GTK| */
+		kde_len = 2 + RSN_SELECTOR_LEN + 2 + gsm->GTK_len +
+			ieee80211w_kde_len(sm);
+	} else {
+		kde_len = wpa_auth_ml_group_kdes_len(sm, KDE_ALL_LINKS);
+	}
+
+	*eid++ = 1 + WPA_KEY_RSC_LEN + kde_len;
+	*eid++ = WLAN_EID_EXT_KEY_DELIVERY;
+
+	/* RSC */
+	if (!is_ml && sm->group->wpa_group_state == WPA_GROUP_SETKEYSDONE)
+		wpa_auth_get_seqnum(sm->wpa_auth, NULL, gsm->GN, rsc);
+	os_memcpy(eid, rsc, WPA_KEY_RSC_LEN);
+	eid += WPA_KEY_RSC_LEN;
+
+	if (is_ml) {
+		eid = wpa_auth_ml_group_kdes(sm, eid, KDE_ALL_LINKS);
+	} else {
+		gtk = gsm->GTK[gsm->GN - 1];
+		gtk_len = gsm->GTK_len;
+		hdr[0] = gsm->GN & 0x03;
+		eid = wpa_add_kde(eid, RSN_KEY_DATA_GROUPKEY, hdr, 2,
+				  gtk, gtk_len);
+		eid = ieee80211w_kde_add(sm, eid);
+	}
+
+	return eid;
+}
+
+
+u8 * wpa_auth_write_assoc_resp_eppke(struct wpa_state_machine *sm,
+				     u8 *pos, size_t max_len, bool is_ml)
+{
+	int res;
+	u8 *end = pos + max_len;
+
+	if (!sm)
+		return pos;
+
+	res = wpa_write_rsn_ie(&sm->wpa_auth->conf, pos, max_len, NULL);
+	if (res < 0)
+		return pos;
+	pos += res;
+	pos = wpa_auth_eid_key_delivery(pos, end - pos, sm, is_ml);
+
+	return pos;
+}
+
+#endif /* CONFIG_ENC_ASSOC */
+
+
 void wpa_reset_assoc_sm_info(struct wpa_state_machine *assoc_sm,
 			     struct wpa_authenticator *wpa_auth,
 			     u8 mld_assoc_link_id)

src/ap/ieee802_11.c
@@ -5731,6 +5731,16 @@ rsnxe_done:
 	}
 #endif /* CONFIG_OWE */
 
+#ifdef CONFIG_ENC_ASSOC
+	if (sta && sta->auth_alg == WLAN_AUTH_EPPKE &&
+	    status_code == WLAN_STATUS_SUCCESS) {
+		reply->frame_control |= WLAN_FC_PROTECTED;
+		p = wpa_auth_write_assoc_resp_eppke(sta->wpa_sm, p,
+						    buf + buflen - p,
+						    ap_sta_is_mld(hapd, sta));
+	}
+#endif /* CONFIG_ENC_ASSOC */
+
 #ifdef CONFIG_DPP2
 	if (DPP_VERSION > 1 && (hapd->conf->wpa_key_mgmt & WPA_KEY_MGMT_DPP) &&
 	    sta && sta->dpp_pfs && status_code == WLAN_STATUS_SUCCESS &&

src/ap/wpa_auth.h
@@ -659,6 +659,12 @@ void wpa_auth_set_ssid_protection(struct wpa_state_machine *sm, bool val);
 void wpa_auth_set_transition_disable(struct wpa_authenticator *wpa_auth,
 				     u8 val);
 
+u8 * wpa_auth_eid_key_delivery(u8 *eid, size_t max_len,
+			       struct wpa_state_machine *sm,
+			       bool is_ml);
+u8 * wpa_auth_write_assoc_resp_eppke(struct wpa_state_machine *sm,
+				     u8 *pos, size_t max_len, bool is_ml);
+
 int wpa_auth_resend_m1(struct wpa_state_machine *sm, int change_anonce,
 		       void (*cb)(void *ctx1, void *ctx2),
 		       void *ctx1, void *ctx2);

CodeLinaro-mirror/le_qca_qca-hostap

commit 582c1697463f367ab3c5c0a6a7021cff7ca57a5e

EPPKE: Fix uninitialized octets in GTK KDE header
The GTK KDE header has two octets and only one of them was explicitly
initialized. Set the other octet to 0 to avoid use of uninitialized
memory.

Fixes: 2a9129858375 ("EPPKE: RSNE/Key Delivery element in (Re)Association Response frame")
Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

src/ap/wpa_auth.c
@@ -8079,6 +8079,7 @@ u8 * wpa_auth_eid_key_delivery(u8 *eid, size_t max_len,
 		gtk = gsm->GTK[gsm->GN - 1];
 		gtk_len = gsm->GTK_len;
 		hdr[0] = gsm->GN & 0x03; /* KeyID bits */
+		hdr[1] = 0;
 		kde = wpa_add_kde(kde, RSN_KEY_DATA_GROUPKEY, hdr, sizeof(hdr),
 				  gtk, gtk_len);
 		kde = ieee80211w_kde_add(sm, kde);