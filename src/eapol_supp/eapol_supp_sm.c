cschuber/hostap

commit c1b3db687e9cbc2de242120adf575df427dc910d

EAP-AUTH: Derive PTK in IEEE 802.1X Authentication alg for non-AP STA
Derive the PTK during IEEE 802.1X authentication when needed to
enable use of association frame encryption.

If PMKSA caching is supported, the PMK is retrieved from the cache.
Otherwise, the MSK from the EAP exchange is used to calculate the PMK.

Derive PTK using the PMK as specified in IEEE P802.11bi/D4.0, 12.7.1.3
(Pairwise key hierarchy), i.e., with DHss added to the context data.

Signed-off-by: Kavita Kavita <kavita.kavita@oss.qualcomm.com>

src/rsn_supp/wpa.c
@@ -7782,9 +7782,23 @@ bool wpa_sm_pmksa_privacy_supported(struct wpa_sm *sm)
 
 
 #ifdef CONFIG_IEEE8021X_AUTH
+
 void wpa_sm_set_802_1x_auth_caps(struct wpa_sm *sm, u64 flags2)
 {
 	sm->eap_over_auth_frame =
 		!!(flags2 & WPA_DRIVER_FLAGS2_802_1X_AUTH);
 }
+
+
+const u8 * wpa_sm_get_pmk(struct wpa_sm *sm, const u8 *addr, const u8 *pmkid,
+			  size_t *pmk_len)
+{
+	if (wpa_supplicant_get_pmk(sm, addr, pmkid) < 0 ||
+	    sm->pmk_len == 0)
+		return NULL;
+
+	*pmk_len = sm->pmk_len;
+	return sm->pmk;
+}
+
 #endif /* CONFIG_IEEE8021X_AUTH */

src/rsn_supp/wpa.h
@@ -703,5 +703,7 @@ int wpa_sm_install_mlo_group_keys(struct wpa_sm *sm, const u8 *key_data,
 bool wpa_sm_pmksa_privacy_supported(struct wpa_sm *sm);
 
 void wpa_sm_set_802_1x_auth_caps(struct wpa_sm *sm, u64 flags2);
+const u8 * wpa_sm_get_pmk(struct wpa_sm *sm, const u8 *addr, const u8 *pmkid,
+			  size_t *pmk_len);
 
 #endif /* WPA_H */

wpa_supplicant/sme.c
@@ -2796,6 +2796,23 @@ static int sme_parse_802_1x_auth_frame(struct wpa_supplicant *wpa_s,
 }
 
 
+static size_t wpas_get_kdk_len(struct wpa_supplicant *wpa_s)
+{
+	const u8 *rsnxe;
+
+	if (!(wpa_s->drv_flags2 & WPA_DRIVER_FLAGS2_SEC_LTF_STA) ||
+	    !wpa_s->current_bss)
+		return 0;
+
+	rsnxe = wpa_bss_get_ie(wpa_s->current_bss, WLAN_EID_RSNX);
+
+	if (rsnxe && ieee802_11_rsnx_capab(rsnxe, WLAN_RSNX_CAPAB_SECURE_LTF))
+		return WPA_KDK_MAX_LEN;
+
+	return 0;
+}
+
+
 static void sme_process_802_1x_auth_response(struct wpa_supplicant *wpa_s,
 					     union wpa_event_data *data)
 {
@@ -2906,6 +2923,47 @@ static void sme_process_802_1x_auth_response(struct wpa_supplicant *wpa_s,
 
 	if (data->auth.status_code == WLAN_STATUS_802_1_X_AUTH_SUCCESS ||
 	    wpa_s->auth_1x->pmkid_found) {
+		if (wpa_s->auth_1x->derive_ptk) {
+			const u8 *peer_addr = wpa_s->valid_links ?
+				wpa_s->ap_mld_addr : wpa_s->pending_bssid;
+			struct wpa_ptk ptk;
+			const u8 *pmk;
+			size_t pmk_len;
+			size_t kdk_len;
+
+			pmk = wpa_sm_get_pmk(wpa_s->wpa, peer_addr,
+					     wpa_s->auth_1x->pmkid_found ?
+					     wpa_s->auth_1x->pmkid :
+					     NULL, &pmk_len);
+			if (!pmk) {
+				wpa_msg(wpa_s, MSG_INFO,
+					"IEEE 802.1X: Failed to get PMK");
+				goto fail;
+			}
+
+			kdk_len = wpas_get_kdk_len(wpa_s);
+
+			if (!wpa_s->auth_1x->dhss ||
+			    wpa_auth_802_1x_pmk_to_ptk(
+				    pmk, pmk_len, wpa_s->own_addr, peer_addr,
+				    wpa_s->auth_1x->snonce,
+				    wpa_s->auth_1x->anonce,
+				    wpa_s->key_mgmt, wpa_s->pairwise_cipher,
+				    wpabuf_head(wpa_s->auth_1x->dhss),
+				    wpabuf_len(wpa_s->auth_1x->dhss),
+				    &ptk, kdk_len) < 0) {
+				wpa_msg(wpa_s, MSG_INFO,
+					"SME: PTK derivation failed");
+				goto fail;
+			}
+
+			/* Clear DHss after successful PTK derivation */
+			wpabuf_clear_free(wpa_s->auth_1x->dhss);
+			wpa_s->auth_1x->dhss = NULL;
+
+			forced_memzero(&ptk, sizeof(ptk));
+		}
+
 		wpa_msg(wpa_s, MSG_INFO,
 			"IEEE 802.1X: Authentication successful");
 

src/common/wpa_common.c
@@ -487,6 +487,8 @@ int wpa_eapol_key_mic(const u8 *key, size_t key_len, int akmp,
  * @ptk: Buffer for pairwise transient key
  * @akmp: Negotiated AKM
  * @cipher: Negotiated pairwise cipher
+ * @z: Additional context data
+ * @z_len: Length of additional context data
  * @kdk_len: The length in octets that should be derived for KDK
  * Returns: 0 on success, -1 on failure
  *
@@ -497,7 +499,9 @@ int wpa_eapol_key_mic(const u8 *key, size_t key_len, int akmp,
  *             [ || Z.x ])
  *
  * The optional Z.x component is used only with DPP and that part is not defined
- * in IEEE 802.11.
+ * in IEEE 802.11. The additional context data is also used to carry the DHss
+ * (Diffie-Hellman shared secret) when IEEE 802.1X authentication in
+ * Authentication frames is  used.
  */
 int wpa_pmk_to_ptk(const u8 *pmk, size_t pmk_len, const char *label,
 		   const u8 *addr1, const u8 *addr2,

CodeLinaro-mirror/le_qca_qca-hostap

commit 72947de47ebab3748283eabf1459ba9aebad7288

EAP-AUTH: Fix PMK retrieval for IEEE 802.1X Authentication Protocol
In SME-in-driver mode, the driver performs candidate selection
independently and may select a different BSS than the one chosen by
wpa_supplicant.

At present, wpa_sm_get_pmk() is used to retrieve the PMK for the IEEE
802.1X authentication protocol. It internally calls
wpa_supplicant_get_pmk() which uses sm->key_mgmt and sm->network_ctx for
PMKSA cache lookup and pmksa_cache_add(). If the driver selects a
different candidate than the one chosen by wpa_supplicant, this can
cause a PMK mismatch.

Add a new helper function sme_get_pmk_for_1x_auth() to retrieve the
PMK with correct parameters for this path.

Remove wpa_sm_get_pmk() and its declaration since this path no longer
uses wpa_supplicant_get_pmk(). Remove the two CONFIG_IEEE8021X_AUTH
blocks from wpa_supplicant_get_pmk() since that path will not be used.

Fixes: c1b3db687e9c ("EAP-AUTH: Derive PTK in IEEE 802.1X Authentication alg for non-AP STA")
Signed-off-by: Kavita Kavita <kavita.kavita@oss.qualcomm.com>

src/eapol_supp/eapol_supp_sm.c
@@ -2339,14 +2339,6 @@ void eapol_sm_set_eap_over_auth_frame(struct eapol_sm *sm, bool active)
 }
 
 
-bool eapol_sm_get_eap_over_auth_frame(struct eapol_sm *sm)
-{
-	if (!sm)
-		return false;
-	return sm->eap_over_auth_frame;
-}
-
-
 struct wpabuf * eapol_sm_get_eapol_pdu(struct eapol_sm *sm, u8 type)
 {
 	struct wpabuf *buf = NULL, *out = NULL;

src/eapol_supp/eapol_supp_sm.h
@@ -374,7 +374,6 @@ int eapol_sm_get_erp_info(struct eapol_sm *sm, struct eap_peer_config *config,
 			  u16 *erp_next_seq_num, const u8 **rrk,
 			  size_t *rrk_len);
 void eapol_sm_set_eap_over_auth_frame(struct eapol_sm *sm, bool active);
-bool eapol_sm_get_eap_over_auth_frame(struct eapol_sm *sm);
 struct wpabuf * eapol_sm_get_eapol_pdu(struct eapol_sm *sm, uint8_t type);
 bool eapol_sm_get_success(struct eapol_sm *sm);
 bool eapol_sm_get_failure(struct eapol_sm *sm);

src/rsn_supp/wpa.c
@@ -429,20 +429,13 @@ static int wpa_supplicant_get_pmk(struct wpa_sm *sm,
 			if (sm->proto == WPA_PROTO_RSN &&
 			    !wpa_key_mgmt_suite_b(sm->key_mgmt) &&
 			    !wpa_key_mgmt_ft(sm->key_mgmt)) {
-				u16 auth_alg = 0;
-
-#ifdef CONFIG_IEEE8021X_AUTH
-				if (eapol_sm_get_eap_over_auth_frame(sm->eapol))
-					auth_alg = WLAN_AUTH_802_1X;
-#endif /* CONFIG_IEEE8021X_AUTH */
-
 				sa = pmksa_cache_add(sm->pmksa,
 						     sm->pmk, pmk_len, NULL,
 						     NULL, 0,
 						     src_addr, sm->own_addr,
 						     sm->network_ctx,
 						     sm->key_mgmt,
-						     fils_cache_id, auth_alg);
+						     fils_cache_id, 0);
 			}
 			if (!sm->cur_pmksa && pmkid &&
 			    pmksa_cache_get(sm->pmksa, src_addr, sm->own_addr,
@@ -491,14 +484,6 @@ static int wpa_supplicant_get_pmk(struct wpa_sm *sm,
 	if (abort_cached && wpa_key_mgmt_wpa_ieee8021x(sm->key_mgmt) &&
 	    !wpa_key_mgmt_suite_b(sm->key_mgmt) &&
 	    !wpa_key_mgmt_ft(sm->key_mgmt)) {
-#ifdef CONFIG_IEEE8021X_AUTH
-		if (eapol_sm_get_eap_over_auth_frame(sm->eapol)) {
-			wpa_printf(MSG_DEBUG,
-				   "RSN: EAP over auth frame - skip EAPOL-Start");
-			return 0;
-		}
-#endif /* CONFIG_IEEE8021X_AUTH */
-
 		/* Send EAPOL-Start to trigger full EAP authentication. */
 		u8 *buf;
 		size_t buflen;
@@ -7799,18 +7784,6 @@ void wpa_sm_set_802_1x_auth_caps(struct wpa_sm *sm, u64 flags2)
 {
 	sm->eap_over_auth_frame =
 		!!(flags2 & WPA_DRIVER_FLAGS2_802_1X_AUTH);
-}
-
-
-const u8 * wpa_sm_get_pmk(struct wpa_sm *sm, const u8 *addr, const u8 *pmkid,
-			  size_t *pmk_len)
-{
-	if (wpa_supplicant_get_pmk(sm, addr, pmkid) < 0 ||
-	    sm->pmk_len == 0)
-		return NULL;
-
-	*pmk_len = sm->pmk_len;
-	return sm->pmk;
 }
 
 #endif /* CONFIG_IEEE8021X_AUTH */

src/rsn_supp/wpa.h
@@ -711,7 +711,5 @@ int wpa_sm_install_mlo_group_keys(struct wpa_sm *sm, const u8 *key_data,
 bool wpa_sm_pmksa_privacy_supported(struct wpa_sm *sm);
 
 void wpa_sm_set_802_1x_auth_caps(struct wpa_sm *sm, u64 flags2);
-const u8 * wpa_sm_get_pmk(struct wpa_sm *sm, const u8 *addr, const u8 *pmkid,
-			  size_t *pmk_len);
 
 #endif /* WPA_H */

wpa_supplicant/sme.c
@@ -113,6 +113,66 @@ static bool sme_is_ml_auth(struct wpa_supplicant *wpa_s, bool external)
 	return external ? wpa_s->sme.ext_ml_auth : wpa_s->valid_links;
 }
 
+
+static int sme_get_pmk_for_1x_auth(struct wpa_supplicant *wpa_s,
+				    const u8 *pmkid, bool external,
+				    u8 *pmk_buf, size_t *pmk_len)
+{
+	struct rsn_pmksa_cache *pmksa_cache;
+	struct rsn_pmksa_cache_entry *entry;
+	const u8 *peer_addr;
+	struct wpa_ssid *ssid;
+	int key_mgmt;
+	size_t len;
+	int res;
+
+	peer_addr = sme_get_peer_addr(wpa_s, external);
+	key_mgmt = sme_get_key_mgmt(wpa_s, external);
+	ssid = wpa_s->sme.ext_auth_wpa_ssid ?
+		wpa_s->sme.ext_auth_wpa_ssid : wpa_s->current_ssid;
+
+	pmksa_cache = wpa_sm_get_pmksa_cache(wpa_s->wpa);
+	if (!pmksa_cache) {
+		wpa_msg(wpa_s, MSG_DEBUG,
+			"IEEE 802.1X: No PMKSA cache available");
+		return -1;
+	}
+
+	if (pmkid) {
+		entry = pmksa_cache_get(pmksa_cache, peer_addr,	wpa_s->own_addr,
+					pmkid, ssid, key_mgmt);
+		if (!entry) {
+			wpa_msg(wpa_s, MSG_DEBUG,
+				"IEEE 802.1X: No PMKSA cache entry found for the given PMKID");
+			return -1;
+		}
+
+		os_memcpy(pmk_buf, entry->pmk, entry->pmk_len);
+		*pmk_len = entry->pmk_len;
+		return 0;
+	}
+
+	if (wpa_key_mgmt_sha384(key_mgmt))
+		len = PMK_LEN_SUITE_B_192;
+	else
+		len = PMK_LEN;
+
+	res = eapol_sm_get_key(wpa_s->eapol, pmk_buf, len);
+	if (res) {
+		wpa_msg(wpa_s, MSG_INFO,
+			"IEEE 802.1X: Failed to get MSK from EAP state machine");
+		return -1;
+	}
+
+	*pmk_len = len;
+
+	pmksa_cache_add(pmksa_cache, pmk_buf, len, NULL, NULL, 0, peer_addr,
+			wpa_s->own_addr, ssid, key_mgmt, NULL,
+			WLAN_AUTH_802_1X);
+
+	return 0;
+}
+
 #endif /* CONFIG_IEEE8021X_AUTH */
 
 
@@ -3685,7 +3745,7 @@ static void sme_process_802_1x_auth_response(struct wpa_supplicant *wpa_s,
 	    wpa_s->auth_1x->pmkid_found) {
 		if (wpa_s->auth_1x->derive_ptk) {
 			struct wpa_ptk ptk;
-			const u8 *pmk;
+			u8 pmk[PMK_LEN_MAX];
 			size_t pmk_len;
 			size_t kdk_len;
 			static const u8 zero[6] = { 0 };
@@ -3694,11 +3754,11 @@ static void sme_process_802_1x_auth_response(struct wpa_supplicant *wpa_s,
 
 			pairwise_cipher = sme_get_pairwise_cipher(wpa_s,
 								  external);
-			pmk = wpa_sm_get_pmk(wpa_s->wpa, peer_addr,
-					     wpa_s->auth_1x->pmkid_found ?
-					     wpa_s->auth_1x->pmkid :
-					     NULL, &pmk_len);
-			if (!pmk) {
+			if (sme_get_pmk_for_1x_auth(
+				    wpa_s,
+				    wpa_s->auth_1x->pmkid_found ?
+				    wpa_s->auth_1x->pmkid : NULL,
+				    external, pmk, &pmk_len) < 0) {
 				wpa_msg(wpa_s, MSG_INFO,
 					"IEEE 802.1X: Failed to get PMK");
 				goto fail;
@@ -3717,8 +3777,11 @@ static void sme_process_802_1x_auth_response(struct wpa_supplicant *wpa_s,
 				    &ptk, kdk_len) < 0) {
 				wpa_msg(wpa_s, MSG_INFO,
 					"SME: PTK derivation failed");
+				forced_memzero(pmk, PMK_LEN_MAX);
 				goto fail;
 			}
+
+			forced_memzero(pmk, PMK_LEN_MAX);
 
 			/* Clear DHss after successful PTK derivation */
 			wpabuf_clear_free(wpa_s->auth_1x->dhss);