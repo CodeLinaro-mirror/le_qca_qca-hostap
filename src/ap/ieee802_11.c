cschuber/hostap

commit 1a8a59f9b2fc4d5996483adec2b2e017bf374e94

EAP-AUTH: Include MIC element in final Authentication frame
Per IEEE P802.11bi/D4.0, 12.16.8.3 (IEEE 802.1X), if a PMKSA is
identified via a PMKID in the RSNE in the first Authentication frame,
the AP shall include a MIC element in the second Authentication frame.

If a PMKSA is not identified through PMKSA caching, the Authentication
frame carrying an EAP-Success, shall include a MIC element.

Signed-off-by: Sai Pratyusha Magam <sai.magam@oss.qualcomm.com>

src/ap/ieee802_11.c
@@ -362,18 +362,36 @@ static int send_auth_reply(struct hostapd_data *hapd, struct sta_info *sta,
 	int reply_res = WLAN_STATUS_UNSPECIFIED_FAILURE;
 	const u8 *sa = hapd->own_addr;
 	struct wpabuf *ml_resp = NULL;
+	size_t ml_resp_len = 0;
+#ifdef CONFIG_IEEE8021X_AUTH
+	size_t mic_len = 0;
+#endif /* CONFIG_IEEE8021X_AUTH */
 
 #ifdef CONFIG_IEEE80211BE
 	if (ap_sta_is_mld(hapd, sta)) {
 		ml_resp = hostapd_ml_auth_resp(hapd);
 		if (!ml_resp)
 			return -1;
+		ml_resp_len = wpabuf_len(ml_resp);
 	}
 #endif /* CONFIG_IEEE80211BE */
 
-	rlen = IEEE80211_HDRLEN + sizeof(reply->u.auth) + ies_len;
-	if (ml_resp)
-		rlen += wpabuf_len(ml_resp);
+	rlen = IEEE80211_HDRLEN + sizeof(reply->u.auth) + ies_len + ml_resp_len;
+#ifdef CONFIG_IEEE8021X_AUTH
+	/* Add MIC element for an Authentication frame carrying an EAP-Success
+	 * message and for an Authentication frame with transaction sequence
+	 * frame 2, if PMKSA caching was used.
+	 */
+	if (auth_alg == WLAN_AUTH_802_1X &&
+	    (resp == WLAN_STATUS_802_1_X_AUTH_SUCCESS ||
+	     sta->eap_auth_data.add_mic)) {
+		mic_len = wpa_mic_len(sta->eap_auth_data.akm,
+				      sta->eap_auth_data.pmk_len,
+				      RSN_HASH_NOT_SPECIFIED);
+		rlen += 2 + mic_len;
+	}
+#endif /* CONFIG_IEEE8021X_AUTH */
+
 	buf = os_zalloc(rlen);
 	if (!buf) {
 		wpabuf_free(ml_resp);
@@ -438,6 +456,59 @@ static int send_auth_reply(struct hostapd_data *hapd, struct sta_info *sta,
 	}
 #endif /* CONFIG_SAE */
 #endif /* CONFIG_TESTING_OPTIONS */
+
+#ifdef CONFIG_IEEE8021X_AUTH
+	if (auth_alg == WLAN_AUTH_802_1X &&
+	    (resp == WLAN_STATUS_802_1_X_AUTH_SUCCESS ||
+	     sta->eap_auth_data.add_mic)) {
+		const u8 *frame, *data, *rsne, *rsnxe;
+		u8 data_buf[500], mic[WPA_1X_MAX_MIC_LEN];
+		size_t frame_len, data_len;
+		const u8 *aa = sa;
+		u8 *ptr = reply->u.auth.variable + ies_len + ml_resp_len;
+
+#ifdef CONFIG_IEEE80211BE
+		if (ap_sta_is_mld(hapd, sta))
+			aa = hapd->mld->mld_addr;
+#endif /* CONFIG_IEEE80211BE */
+
+		rsne = hostapd_wpa_ie(hapd, WLAN_EID_RSN);
+		if (!rsne) {
+			wpa_printf(MSG_INFO, "No AP RSNE");
+			return -1;
+		}
+		os_memcpy(data_buf, rsne, 2 + rsne[1]);
+		data_len = 2 + rsne[1];
+
+		rsnxe = hostapd_wpa_ie(hapd, WLAN_EID_RSNX);
+		if (rsnxe) {
+			wpa_printf(MSG_DEBUG, "Found AP RSNXE");
+			os_memcpy(&data_buf[data_len], rsnxe, 2 + rsnxe[1]);
+			data_len += 2 + rsnxe[1];
+		} else {
+			wpa_printf(MSG_DEBUG, "No AP RSNXE");
+		}
+		data = data_buf;
+
+		/* MIC element */
+		ptr[0] = WLAN_EID_MIC;
+		ptr[1] = mic_len;
+		os_memset(ptr + 2, 0, mic_len);
+
+		frame = (const u8 *) &reply->u.auth.auth_alg;
+		frame_len =  rlen - IEEE80211_HDRLEN;
+		if (wpa_auth_8021x_mic(sta->eap_auth_data.akm,
+				       sta->eap_auth_data.ptk.kck,
+				       sta->eap_auth_data.ptk.kck_len,
+				       aa, sta->addr, data, data_len,
+				       frame, frame_len, mic)) {
+			wpa_printf(MSG_INFO, "Failed to derive MIC");
+			return -1;
+		}
+		os_memcpy(ptr + 2, mic, mic_len);
+	}
+#endif /* CONFIF_IEEE8021X_AUTH */
+
 	if (hostapd_drv_send_mlme(hapd, reply, rlen, 0, NULL, 0, 0) < 0)
 		wpa_printf(MSG_INFO, "send_auth_reply: send failed");
 	else
@@ -2739,6 +2810,7 @@ static void handle_auth_802_1x(struct hostapd_data *hapd, struct sta_info *sta,
 			}
 
 			sta->flags |= WLAN_STA_AUTH;
+			sta->eap_auth_data.add_mic = true;
 			send_8021x_auth_reply(hapd, sta, auth_transaction + 1,
 					      WLAN_STATUS_SUCCESS, reply);
 			/* Delete DHss after successful PTK derivation */

src/ap/sta_info.h
@@ -84,6 +84,7 @@ struct eap_over_auth_data {
 	size_t rsnxe_len;
 	struct crypto_ecdh *ecdh;
 	struct wpabuf *dhss;
+	bool add_mic;
 };
 
 #define EHT_ML_MAX_STA_PROF_LEN 1024

src/common/wpa_common.h
@@ -25,6 +25,7 @@
 #define WPA_PASN_PMK_LEN 32
 #define WPA_PASN_MAX_MIC_LEN 32
 #define WPA_MAX_RSNXE_LEN 4
+#define WPA_1X_MAX_MIC_LEN 32
 
 #define OWE_DH_GROUP 19
 

CodeLinaro-mirror/le_qca_qca-hostap

commit bcbd0b43ec79b8c765acede411730c5c39382f32

EAP-AUTH: Make sure STA entry exists before accessing it in send_auth_reply()
handle_auth() can invoke send_auth_reply() before a STA entry has been
allocated for authentication processing failure scenarios.

In IEEE 8021.X authentication utilizing Authentication frames,
send_auth_reply() might access eap_auth_data before a valid STA entry is
available. Verify that a STA entry exists before accessing it.

Fixes: 1a8a59f9b2fc ("EAP-AUTH: Include MIC element in final Authentication frame")
Signed-off-by: Sai Pratyusha Magam <sai.magam@oss.qualcomm.com>

src/ap/ieee802_11.c
@@ -391,7 +391,7 @@ static int send_auth_reply(struct hostapd_data *hapd, struct sta_info *sta,
 	 */
 	if (auth_alg == WLAN_AUTH_802_1X &&
 	    (resp == WLAN_STATUS_802_1_X_AUTH_SUCCESS ||
-	     sta->eap_auth_data.add_mic)) {
+	     (sta && sta->eap_auth_data.add_mic))) {
 		mic_len = wpa_mic_len(sta->eap_auth_data.akm,
 				      sta->eap_auth_data.pmk_len,
 				      RSN_HASH_NOT_SPECIFIED);
@@ -467,7 +467,7 @@ static int send_auth_reply(struct hostapd_data *hapd, struct sta_info *sta,
 #ifdef CONFIG_IEEE8021X_AUTH
 	if (auth_alg == WLAN_AUTH_802_1X &&
 	    (resp == WLAN_STATUS_802_1_X_AUTH_SUCCESS ||
-	     sta->eap_auth_data.add_mic)) {
+	     (sta && sta->eap_auth_data.add_mic))) {
 		const u8 *frame, *data, *rsne, *rsnxe;
 		u8 data_buf[500], mic[WPA_1X_MAX_MIC_LEN];
 		size_t frame_len, data_len;